8bc06a2cc813e28653795ab2ddb0f1ff0c664635
Michael Getachew <mikegeta79@gmail.com>
2021-03-03 23:04:25 -0500

    updateed

--- public/script.js
+++ public/script.js
@@ -6,8 +6,11 @@ const socket = io("/");
 const myPeer = new Peer(undefined, {
     path: "/peerjs",
     host: "/",
-    port: "3000",
-})
+    port: "3030",
+});
+
+const peers = {};
+
 
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
@@ -15,25 +18,57 @@ myVideo.muted =true
 
 let myVideoStream;
 
-navigator.mediaDevices.getUserMedia({
+navigator.mediaDevices
+  .getUserMedia({
     video: true,
     audio: false,
-})
+  })
     .then((stream) => {
         myVideoStream = stream;
         addVideoStream(myVideo, stream);
-    });
 
+        myPeer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
+
+        socket.on("user_connected", (userId) => {
+            setTimeout(function () {
+                connectToNewUser(userId, stream);
+            }, 1000);
+      
+        });
+
+        let text = $("input");
+        $("html").keydown(function (e) {
+        if (e.which == 13 && text.val().length !== 0) {
+        socket.emit("message", text.val());
+        text.val("");
+        }
+        
+    })
+    socket.on("createMessage", (message) => {
+      $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+   
+    });
+  });
+        
 myPeer.on("open", (id) => {
     socket.emit("join-room", ROOM_ID, id);
-
-})
-
-
-
-socket.on("user_connected", () => {
-  connectToNewUser(userId);
 });
+
+socket.on("user-disconnected", (userId) => {
+    if (peers[userId]) {
+        peers[userId].close();
+    }
+});
+
+
+
 
 
     function addVideoStream(video, stream) {
@@ -44,6 +79,16 @@ socket.on("user_connected", () => {
       videoGrid.append(video);
 }
     
-function connectToNewUser(userId) {
-    console.log("mike front", userId)
+function connectToNewUser(userId,stream ) {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    });
+    call.on("close", () => {
+        video.remove();
+    });
+
+
+    peers[userId] = call;
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
-const { Socket } = require("dgram");
+// const { Socket } = require("dgram");
 const express = require("express");
-const { request } = require("https");
+// const { request } = require("https");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server)
@@ -24,15 +24,22 @@ app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room });
 });
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
     socket.on("join-room", (roomId, userId) => {
         socket.join(roomId);
-        socket.to(roomId).broadcast.emit("user-connected", userId);
-    });
+        socket.to(roomId).broadcast.emit("user_connected", userId);
+
+    socket.on("message", (message) => {
+    io.to(roomId).emit("creaateMessage", message);
+        });
+
+        socket.on("disconnect", () => {
+            socket.to(roomId).broadcast.emit("user_disconnected", userId);
+        });
+
+    })
 
 });
-
-
 
 
 server.listen(process.env.PORT || 3000);
