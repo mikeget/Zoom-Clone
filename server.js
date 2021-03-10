8b3856eb65e8d502150c30a5f58650e78faf65e4
Michael Getachew <mikegeta79@gmail.com>
2021-03-09 22:01:04 -0500

    updated

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
 
-// const { Socket } = require("dgram");
-const express = require("express");
-// const { request } = require("https");
-const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server)
-const { v4: uuidV4 } = require("uuid");
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
+    const express = require("express");
+    const app = express();
+    // const cors = require('cors')
+    // app.use(cors())
+    const server = require("http").Server(app);
+    const io = require("socket.io")(server);
+    const { ExpressPeerServer } = require("peer");
+    const peerServer = ExpressPeerServer(server, {
     debug: true,
-});
+    });
+    const { v4: uuidV4 } = require("uuid");
 
+    app.use("/peerjs", peerServer);
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use("/peerjs", peerServer);
+    app.set("view engine", "ejs");
+    app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-res.redirect(`/${uuidV4()}`);
-});
+    app.get("/", (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+    });
 
-app.get("/:room", (req, res) => {
+    app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room });
-});
+    });
 
-io.on("connection", socket => {
+    io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId) => {
         socket.join(roomId);
-        socket.to(roomId).broadcast.emit("user_connected", userId);
-
-    socket.on("message", (message) => {
-    io.to(roomId).emit("creaateMessage", message);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+    
+        socket.on("message", (message) => {
+    
+        io.to(roomId).emit("createMessage", message);
         });
 
         socket.on("disconnect", () => {
-            socket.to(roomId).broadcast.emit("user_disconnected", userId);
+        socket.to(roomId).broadcast.emit("user-disconnected", userId);
         });
+    });
+    });
 
-    })
-
-});
-
-
-server.listen(process.env.PORT || 3000);
+    server.listen(process.env.PORT || 3000);
